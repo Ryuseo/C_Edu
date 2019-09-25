b8325dad2d87821554846d35aceaa81e7abd18f2
Ryuseo <rkd4apf@gmail.com>
2019-09-25 17:52:24 +0900

    05 수식과 연산자 2
    형변환 예제에서 float와 double가 똑같이 출력 되는 문제 해결
    관계 연산자
    논리 연산자
    비트 연산자 예제 작성

--- C_Edu/05_math.cpp
+++ C_Edu/05_math.cpp
@@ -13,6 +13,9 @@ void play_05()
 		printf("2. 증감연산자 예제\n");
 		printf("3. 복합 대입연산자 예제\n");
 		printf("4. 명시적 형변환 예제\n");
+		printf("5. 관계 연산자 예제\n");
+		printf("6. 논리 연산자 예제\n");
+		printf("7. 비트 연산자 예제\n");
 		printf("\n");
 		printf("실행할 예제 번호 입력.(다른 프로그램 실행은 0번) : ");
 		scanf_s("%d", &num);
@@ -37,6 +40,14 @@ void play_05()
 		case 4:
 			explicit_casting();
 			break;
+		case 5:
+			relational_op();
+			break;
+		case 6:
+			logical_op();
+			break;
+		case 7:
+			bit_op();
 		default:
 			printf("없는 예문. 다시 입력");
 			break;
@@ -92,10 +103,90 @@ void compound_assignment_op()
 
 void explicit_casting()
 {
-	double number = 139843767015701275173.14159265358979323846264338327950288419716939937;
-	printf("double pie = %lf\n", number);
+	double number = 128.14159265358979323846264338327950288419716939937;
+	printf("double pie = %lf\n", number);		//왜 %f와 출력 결과가 같을까? -> https://gigglehd.com/zbxe/10649472  기본 인자 진급이라는 것 때문인듯
 	printf("(char)pie = %d\n", (char)number);
 	printf("(int)pie = %d\n", (int)number);
 	printf("(long)pie = %d\n", (long)number);
-	printf("(float)pie = %f\n", (float)number);
+	printf("(float)pie = %f\n", (float)number);	//왜 %lf와 출력 결과가 같을까? -> %lf와 %f는 내부적으로 동일하게 작동한다고 한다. 그냥 scanf에는 %lf가 있는데 printf에는 없어서 헷갈리지 말라고 만들었다고도 한다.
+}
+
+void relational_op()
+{
+	srand(time(NULL));
+	int nums[3][3];
+
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 3; ++j)
+		{
+			nums[i][j] = (rand() % 20) - 10;
+		}
+	}
+	for (int i = 0; i < 3; ++i)//O(N*N*N*N) ㄷㄷ
+	{
+		for (int j = 0; j < 3; ++j)
+		{
+			for (int k = 0; k < 3; ++k)
+			{
+				for (int l = 0; l < 3; ++l)
+				{
+					printf("%d == %d : ", nums[i][j], nums[k][l]);
+					printf(nums[i][j] == nums[k][l] ? "true\n" : "false\n");
+
+					printf("%d != %d : ", nums[i][j], nums[k][l]);
+					printf(nums[i][j] != nums[k][l] ? "true\n" : "false\n");
+
+					printf("%d > %d : ", nums[i][j], nums[k][l]);
+					printf(nums[i][j] > nums[k][l] ? "true\n" : "false\n");
+
+					printf("%d < %d : ", nums[i][j], nums[k][l]);
+					printf(nums[i][j] < nums[k][l] ? "true\n" : "false\n");
+
+					printf("%d >= %d : ", nums[i][j], nums[k][l]);
+					printf(nums[i][j] >= nums[k][l] ? "true\n" : "false\n");
+
+					printf("%d <= %d : ", nums[i][j], nums[k][l]);
+					printf(nums[i][j] <= nums[k][l] ? "true\n" : "false\n");
+					printf("\n");
+				}
+			}
+		}
+		printf("\n");
+	}
+}
+
+void logical_op()
+{
+	bool tf[2] = { true, false };
+	for (int i = 0; i < 2; ++i)
+	{
+		for (int j = 0; j < 2; ++j)
+		{		
+			printf(tf[i] ? "true && " : "false && ");
+			printf(tf[j] ? "true : " : "false : ");
+			printf(tf[i] && tf[j] ? "true\n" : "false\n");
+
+			printf(tf[i] ? "true || " : "false || ");
+			printf(tf[j] ? "true : " : "false : ");
+			printf(tf[i] || tf[j] ? "true\n" : "false\n");
+
+			printf(tf[i] ? "!true : " : "!false : ");
+			printf(!tf[i] ? "true\n" : "false\n");
+
+			printf("\n");
+		}
+	}
+}
+
+void bit_op()
+{
+	int x = 9, y = 10;
+
+	printf("x AND(&) y = %08X\n", x & y);
+	printf("x OR(|) y = %08X\n", x | y);
+	printf("x XOR(^) y= %08X\n", x ^ y);
+	printf("NOT(~) x = %08X\n", ~x);
+	printf("x << 1 = %08X\n", x << 1);
+	printf("x >> 1 = %08X\n", x >> 1);
 }
--- C_Edu/05_math.h
+++ C_Edu/05_math.h
@@ -1,9 +1,14 @@
 #pragma once
 #include <stdio.h>
 #include <Windows.h>
+#include <stdlib.h>
+#include <time.h>
 
 void play_05();
 void arithmetic_op();
 void incdec();
 void compound_assignment_op();
 void explicit_casting();
+void relational_op();
+void logical_op();
+void bit_op();
